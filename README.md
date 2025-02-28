4af4f445edda82b2a20f8b00e4fcaab90bdfb3db
kyoruni <40832190+kyoruni@users.noreply.github.com>
2025-02-28 23:42:53 +0900

    データベースの設定を追加 (#2)
    * :wrench: データベースコンテナの作成 #1
    
    * :wrench: ローカルホスト以外からの接続も許可する #1
    
    * :wrench: タイムゾーンを設定 #1
    
    * :wrench: タイムゾーンやロケールの設定など #1
    
    ロケールはC(デフォルト)にする
    エンコーディングはデフォルトでUTF8だったが、明示的に設定
    DBコンテナ名を変更
    初期化SQLのマウントをcomposeの方に移動(設定はなるべくまとめて書いておきたい)
    
    * :pencil: README修正 #1
    
    * :recycle: 整形 #1

---
+++ README.md
@@ -0,0 +1,13 @@
+# kyoruni/chie-box
+
+## 起動方法
+
+```bash
+$ docker compose up -d
+```
+
+## DB
+
+```bash
+$ docker compose exec db psql -U postgres -d chiebox
+```
---
+++ postgresql/init.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA IF NOT EXISTS app;
+
+CREATE TABLE IF NOT EXISTS "app"."users" (
+    id SERIAL,
+    name       VARCHAR(31) NOT NULL,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY ("id"),
+    UNIQUE ("name"),
+    UNIQUE ("email")
+);
+
+COMMENT ON TABLE  "app"."users"              IS 'ユーザー';
+COMMENT ON COLUMN "app"."users"."id"         IS 'ID';
+COMMENT ON COLUMN "app"."users"."name"       IS 'ユーザー名';
+COMMENT ON COLUMN "app"."users"."email"      IS 'メールアドレス';
+COMMENT ON COLUMN "app"."users"."password"   IS 'パスワード';
+COMMENT ON COLUMN "app"."users"."created_at" IS '作成日時';
+COMMENT ON COLUMN "app"."users"."updated_at" IS '更新日時';
